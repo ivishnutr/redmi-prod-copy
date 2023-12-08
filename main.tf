9d56e7117d05f7507a9a497edbd46654f0aad436
vishnu <vishnu@gmail.com>
2023-12-08 11:03:33 +0000

    8080 port added

--- main.tf
+++ main.tf
@@ -22,6 +22,15 @@ resource "aws_security_group" "http_access" {
   name        = "${var.project_name}-${var.project_env}-http-acess"
   description = "${var.project_name}-${var.project_env}-http-acess"
 
+ingress {
+    description      = "TLS from VPC"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
   ingress {
     description      = "TLS from VPC"
     from_port        = 80
