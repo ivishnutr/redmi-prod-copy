8d62e8523d4753d5685465cc2ad520f30f77a9d0
vishnu <vishnu@gmail.com>
2023-12-08 10:56:04 +0000

    webserver sg created

--- main.tf
+++ main.tf
@@ -13,3 +13,45 @@ resource "aws_key_pair" "auth_key" {
   }
 }
 
+
+#----------------------------------
+# webserver security creation
+#----------------------------------
+
+resource "aws_security_group" "http_access" {
+  name        = "${var.project_name}-${var.project_env}-http-acess"
+  description = "${var.project_name}-${var.project_env}-http-acess"
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-${var.project_env}-http-access"
+    #project = var.project_name
+    #env     = var.project_env
+    #owner   = var.project_owner
+  }
+}
