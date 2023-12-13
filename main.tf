e70f8a5ad01468ebdec72cf651ae8e710eb0dddf
vishnutr <vishnutr@gmail.com>
2023-12-13 08:41:19 +0000

    prometheus port added

--- main.tf
+++ main.tf
@@ -22,6 +22,16 @@ resource "aws_security_group" "http_access" {
   name        = "${var.project_name}-${var.project_env}-http-acess"
   description = "${var.project_name}-${var.project_env}-http-acess"
 
+ ingress {
+    description      = "TLS from VPC"
+    from_port        = 9090
+    to_port          = 9090
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+
 ingress {
     description      = "TLS from VPC"
     from_port        = 8080
